d44bdbc869967b7185a8b44ef13d600f94dbcf0e
Isaac Bernat <isaac.b@preply.com>
2022-07-26 19:18:16 +0200

    006-images use namedtuple for Button

--- tinymem.py
+++ tinymem.py
@@ -1,13 +1,13 @@
-import random, time, thumby
+import random, time, thumby, collections
 
+
+sequence, max_pos, difficulty = [], 0, 0
 controls_map = bytearray([224,32,32,32,32,63,1,1,1,1,1,1,1,1,1,63,32,32,32,32,224,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,255,1,1,1,1,1,1,1,255,
             255,128,128,128,128,128,0,0,0,0,0,0,0,0,0,128,128,128,128,128,255,0,0,0,0,0,0,0,0,0,0,0,252,4,4,4,4,4,4,4,252,0,7,4,4,4,4,4,4,4,7,
             0,0,0,0,0,31,16,16,16,16,16,16,16,16,16,31,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,31,16,16,16,16,16,16,16,31,0,0,0,0,0,0,0,0,0,0])
-controls_sprite = thumby.Sprite(51, 21, controls_map, 10, 10)  # L. Wienclaw
-
-sequence, max_pos, difficulty = [], 0, 0
-MEM_KEYS = ["A", "B", "UP", "RIGHT", "DOWN", "LEFT"]  # TODO bigger onscreen
-freq = [440, 330, 659, 554, 440, 330]  # TODO try around 5000-6000hz
+controls_sprite = thumby.Sprite(51, 21, controls_map, 10, 10)  # sprite based on Laveréna Wienclaw
+Button = collections.namedtuple("Button", "letter freq x y")  # TODO try freq around 5000-6000hz
+MEM_KEYS = [Button("A", 440, 52, 12), Button("B", 330, 42, 22), Button("U", 659, 18, 12), Button("R", 554, 24, 17), Button("D", 440, 18, 22), Button("L", 330, 12, 17)]
 
 
 def print_sprite(clear=True):
@@ -37,11 +37,15 @@ def init_game():
         sequence = [random.randint(2, 5) for i in range(100)]
 
 
-def getcharinputNew():
-    return (thumby.buttonL.justPressed() and 5) or (thumby.buttonD.justPressed() and 4) or (thumby.buttonR.justPressed() and 3) or (thumby.buttonU.justPressed() and 2) or (thumby.buttonB.justPressed() and 1) or (thumby.buttonA.justPressed() and 0) or None
+def getcharinputNew(display=False):
+    val = (thumby.buttonL.justPressed() and 5) or (thumby.buttonD.justPressed() and 4) or (thumby.buttonR.justPressed() and 3) or (thumby.buttonU.justPressed() and 2) or (thumby.buttonB.justPressed() and 1) or (thumby.buttonA.justPressed() and 0) or None
+    if val is not None and display:
+        thumby.display.drawText(MEM_KEYS[val].letter, MEM_KEYS[val].x, MEM_KEYS[val].y, 1)
+        thumby.display.update()
+    return val
 
 
-def wait_press(c=getcharinputNew()):
+def wait_press(c=getcharinputNew(display=False)):
     while(c is None):
         c = getcharinputNew()
     return c
@@ -51,8 +55,8 @@ def print_sequence():
     print_text(["watch key", "sequence", "CAREFULLY", "", "press ANY"])
     wait_press()
     for index, val in enumerate(sequence[:max_pos + 1]):
-        print_text(["" for i in range(index % 4)] + [MEM_KEYS[val]])
-        thumby.audio.playBlocking(freq[val], 1000)
+        print_text(["" for i in range(index % 4)] + [MEM_KEYS[val].letter])
+        thumby.audio.playBlocking(MEM_KEYS[val].freq, 1000)
         init_time = time.ticks_ms()
 
 
@@ -62,18 +66,16 @@ def game_over():
     init_game()
 
 
-def ask_sequence():
+def ask_sequence(current_pos=0):
     print_text(["your turn", "repeat"])
-    current_pos = 0
     while (current_pos <= max_pos):
         if sequence[current_pos] != wait_press():
             game_over()
             break
         thumby.audio.stop()
-        thumby.audio.play(freq[sequence[current_pos]], 1000)
+        thumby.audio.play(MEM_KEYS[sequence[current_pos]].freq, 1000)
         current_pos += 1
         print_text(["correct!", f"{current_pos} done", f"{max_pos - current_pos + 1} left"])
-    return
 
 
 init_game()
